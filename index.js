39ac00f59ae7d65dba112b305669d04cca0ba613
Arnold-O <arnoldoghiator@gmail.com>
2022-05-18 13:41:48 +0100

    update

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 
   
-         const btnOpen = document.querySelector('.first')
+         const btnOpen = document.querySelectorAll('.first')
     
-                 const secondAgleDown = document.querySelector('.secondAgle')
+                 const secondAgleDown = document.querySelectorAll('.secondAgle')
 
-            const selector =document.querySelector('.takeItDown')
+            const selector =document.querySelectorAll('.takeItDown')
+            console.log(selector)
+            console.log(secondAgleDown)
+            console.log(btnOpen)
 
+for(let i = 0; i<selector.length; i++)
+            selector[i].addEventListener('click', ()=>{
+                btnOpen[i].classList.remove('hidden')
+                document.querySelectorAll('.takeItDown')[i].classList.add('hidden')
+                document.querySelectorAll('.the-other-angle')[i].classList.remove('hidden')
+                document.querySelectorAll('.adder')[i].style.color = "green"
+                document.querySelectorAll('.adder')[i].style.fontSize = "20px"
+                document.querySelectorAll('.main-box-border-above')[i].style.borderTop = " 2px solid green"
 
-            selector.addEventListener('click', ()=>{
-                btnOpen.classList.remove('hidden')
-                document.querySelector('.takeItDown').classList.add('hidden')
-                document.querySelector('.the-other-angle').classList.remove('hidden')
-                document.querySelector('.adder').style.color = "green"
-                document.querySelector('.adder').style.fontSize = "20px"
-                document.querySelector('.main-box-border-above').style.borderTop = " 2px solid green"
+            })
+
+            for(let i = 0; i<secondAgleDown.length; i++)
+            secondAgleDown[i].addEventListener('click', ()=>{
+                btnOpen[i].classList.add('hidden')
+                document.querySelectorAll('.takeItDown')[i].classList.remove('hidden')
+                document.querySelectorAll('.the-other-angle')[i].classList.add('hidden')
+                document.querySelectorAll('.adder')[i].style.color = ""
+                document.querySelectorAll('.adder')[i].style.fontSize = "14px"
+                document.querySelectorAll('.main-box-border-above')[i].style.borderTop = ""
 
             })
 
 
-            secondAgleDown.addEventListener('click', ()=>{
-                btnOpen.classList.add('hidden')
-                document.querySelector('.takeItDown').classList.remove('hidden')
-                document.querySelector('.the-other-angle').classList.add('hidden')
-                document.querySelector('.adder').style.color = ""
-                document.querySelector('.adder').style.fontSize = "16px"
-                document.querySelector('.main-box-border-above').style.borderTop = ""
 
-            })
+            
