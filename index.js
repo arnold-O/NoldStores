fb506b7d7255ab706a67fe4ac910782d59a38e9a
Arnold-O <arnoldoghiator@gmail.com>
2022-05-19 23:53:15 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-  
-         const btnOpen = document.querySelectorAll('.first')
-    
+//   faq section----------------------------//////
+const btnOpen = document.querySelectorAll('.first')
+
                  const secondAgleDown = document.querySelectorAll('.secondAgle')
 
             const selector =document.querySelectorAll('.takeItDown')
@@ -17,9 +17,9 @@ for(let i = 0; i<selector.length; i++)
                 document.querySelectorAll('.adder')[i].style.color = "green"
                 document.querySelectorAll('.adder')[i].style.fontSize = "20px"
                 document.querySelectorAll('.main-box-border-above')[i].style.borderTop = " 2px solid green"
-
+                
             })
-
+            
             for(let i = 0; i<secondAgleDown.length; i++)
             secondAgleDown[i].addEventListener('click', ()=>{
                 btnOpen[i].classList.add('hidden')
@@ -31,6 +31,52 @@ for(let i = 0; i<selector.length; i++)
 
             })
 
+            //   faq section----------------------------//////
 
+            
 
+// flash section js logic
+
+const timerDown = document.querySelector('.time-fixing-javascript')
+function countdownTimeStart(){
+
+   const ReferenceDownTime = new Date("May 20, 2022 06:00:00").getTime();
+    
+    // Update the count down every 1 second
+    const x = setInterval(function() {
+    
+        // Get the noe time
+        const now = new Date().getTime();
+        
+        // Find the distance between now an the count down date
+        const timerDown = ReferenceDownTime - now;
+        
+        // Time calculations for days, hours, minutes and seconds
+        const hours = Math.floor((timerDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timerDown % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timerDown % (1000 * 60)) / 1000);
+        
+        
+        document.querySelector(".time-fixing-javascript").textContent =`  ${hours }h ${ minutes}m ${seconds}s  `;
+        
+        
+        if (timerDown < 0) {
+            clearInterval(x);
+            document.querySelector(".time-fixing-javascript").textContent = "EXPIRED";
+        }
+    }, 1000);
+    }
+
+    countdownTimeStart()
+
+// let time = 1600
+
+// setInterval(()=>{
+//     // const hour = String(Math.trunc(time/3600)).padStart(2,0)
+//     const min = String(Math.trunc(time /60)).padStart(2,0)
+//     const sec =String( time % 60).padStart(2, 0)
+//     timerDown.textContent =`${min}:${sec}`
+//     time--
+// },1000)
+// flash section js logic
             
